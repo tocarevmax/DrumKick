4323d031956ee4a9166553cdf62b9e4d74aecab8
Maxim Tocarev <max.tocarev@gmail.com>
2017-11-22 13:31:12 -0800

    Create and style benefit block on splash page

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -33,5 +33,33 @@ export default () => (
          src="//assets.sk-static.com/assets/nw/components/homepage/hero-4-c269994.jpg"
       />
     </div>
+    <div className="benefits">
+      <div className="benefit-block">
+        <img
+          src="//assets.sk-static.com/assets/nw/components/homepage/artist-icon-377d4ea.svg"/>
+        <h4>Track your favorite artists</h4>
+        <p>
+          {"Tell us who you want to see live. Easily import your favorite artists from Spotify, Facebook and more."}
+        </p>
+      </div>
+
+      <div className="benefit-block">
+        <img
+          src="//assets.sk-static.com/assets/nw/components/homepage/calendar-icon-2d97a4a.svg"/>
+        <h4>Get personalized concert alerts</h4>
+        <p>
+          {"We'll email you as soon as your tracked artists announce shows nearby. No spam about bands you don’t care about."}
+        </p>
+      </div>
+
+      <div className="benefit-block">
+        <img
+          src="//assets.sk-static.com/assets/nw/components/homepage/ticket-icon-cc3f1d4.svg"/>
+        <h4>Never miss out on tickets</h4>
+        <p>
+          {"We send concert alerts early, with everything you need to buy tickets. You'll never miss a great concert again."}
+        </p>
+      </div>
+    </div>
   </div>
 );
