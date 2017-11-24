f1664a7d0db08fc56062aeb9d3f7d83c67395a7a
Maxim Tocarev <max.tocarev@gmail.com>
2017-11-24 15:27:21 -0800

    Search bar UI

--- frontend/components/search/search_index.jsx
+++ frontend/components/search/search_index.jsx
@@ -1,34 +1,59 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 
-const SearchIndex = ({search, changeUISearch}) => {
+class SearchIndex extends React.Component {
 
-  const handleUIChange = () => {
-    changeUISearch(false);
-  };
-
-  if (!search.artists) {
-    return (<h1>Nothing to render</h1>);
+  constructor(props) {
+    super(props);
   }
-  return (
-    <div>
-      <h1>Search Results</h1>
-      <h3>Artists:</h3>
-      <ul>
-        {search.artists.map((artist, idx) => (
-          <li key={`artists-key-${idx}`} onClick={handleUIChange}>{artist.name}</li>
-        ))}
-      </ul>
 
-      <h3>Concerts:</h3>
-      <ul>
-        {search.concerts.map((concert, idx) => (
-          <li key={`concerts-key-${idx}`}>{concert.name}</li>
-        ))}
-      </ul>
+  handleUIChange () {
+    this.props.changeUISearch(false);
+  }
 
-    </div>
-  );
-};
+  componentWillUpdate(newProps){
+    if (this.props.location.pathname != newProps.location.pathname) {
+      this.props.changeUISearch(false);
+    }
+  }
 
-export default SearchIndex;
+  render() {
+    if (!this.props.search.artists) {
+      return (<h1>Nothing to render</h1>);
+    }
+    return (
+      <div>
+        <h1>Search Results</h1>
+        <h3>Artists:</h3>
+        <ul>
+          {this.props.search.artists.map((artist, idx) => (
+            <li
+              key={`artists-key-${idx}`}
+              onClick={this.handleUIChange.bind(this)}
+            >
+              {artist.name}
+            </li>
+          ))}
+        </ul>
+
+        <h3>Concerts:</h3>
+        <ul>
+          {this.props.search.concerts.map((concert, idx) => (
+            <li key={`concerts-key-${idx}`}>{concert.name}</li>
+          ))}
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+// const SearchIndex = ({search, changeUISearch}) => {
+//
+//
+//
+//
+// };
+
+export default withRouter(SearchIndex);
